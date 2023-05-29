a7c47c05855a12a0f1b88e828bda70627cbccf77
Luiz Miguel Tavares <luizmiguel.pessotti@gmail.com>
2023-05-29 12:40:58 -0300

    Changing everything

--- src/2d_mapping/2d_mapping/mapping.py
+++ src/2d_mapping/2d_mapping/mapping.py
@@ -1,9 +1,12 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.executors import MultiThreadedExecutor
+from rclpy.executors import MultiThreadedExecutor, SingleThreadedExecutor
+
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan 
+from nav_msgs.msg import OccupancyGrid
+
 from tf_transformations import euler_from_quaternion
 import math
 import pandas as pd
@@ -11,6 +14,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+class MapVisualizer(Node):
+
+    def __init__(self):
+        super().__init__('map_visualizer')
+
+        self.subscription = self.create_subscription(
+            OccupancyGrid,
+            'map_data',
+            self.visualize_map,
+            10
+        )
+
+    def visualize_map(self, msg):
+        if len(msg.data) == 0:
+            self.get_logger().info('Received empty map data. Skipping visualization.')
+            return
+        self.get_logger().info('Received!')
+        occupancy_data = np.array(msg.data)
+
+        width = msg.info.width
+        height = msg.info.height
+        resolution = msg.info.resolution
+
+        # Reshape the occupancy data array to match the grid dimensions
+        occupancy_map = occupancy_data.reshape((height, width))
+
+        # Create a figure and axis for the plot
+        
+        #plt.switch_backend('TkAgg')
+        # Plot the occupancy grid
+        #plt.imshow(occupancy_map, cmap='gray', origin='lower')
+        #plt.show()
 
 class MapCreator(Node):
 
@@ -21,16 +56,18 @@ class MapCreator(Node):
         self.laser_const     = 1
         self.frames_sec      = 10
         self.resolution      = 0.05
-        self.grid_resolution = 0.05
+        self.grid_resolution = 0.005
 
         self.pose = None # Inicializando a pose
         self.ranges = None
+        self.map = None
         
         self.group = ReentrantCallbackGroup()
         self.pose_subscriber = self.create_subscription(Odometry, "odom", self.Turtlebot3_pose, 40, callback_group=self.group)
         self.laser_subscriber = self.create_subscription(LaserScan, "scan", self.laser_reading, 10, callback_group=self.group)
-        self.timer_ = self.create_timer(1/self.frames_sec, self.polar_to_cartesian_global, callback_group=self.group)
-        self.get_logger().info("Mapping Node has been started!!")
+        self.GridMap_publisher = self.create_publisher(OccupancyGrid, 'map_data', 10)
+        self.timer_ = self.create_timer(1/self.frames_sec, self.loop, callback_group=self.group)
+        #self.get_logger().info("Mapping Node has been started!!")
 
     def Turtlebot3_pose(self, msg): # OK
         self.pose = msg
@@ -44,7 +81,7 @@ class MapCreator(Node):
 
         #self.get_logger().info(str(self.pose_timestamp))
         #self.get_logger().info(str(time.perf_counter()))
-        self.get_logger().info('Pose')
+        #self.get_logger().info('Pose')
 
     def laser_reading(self, msg): # OK
         # Sentido anti-horário
@@ -52,12 +89,12 @@ class MapCreator(Node):
         self.laser_timestamp = msg.header.stamp.sec + msg.header.stamp.nanosec*10e-9
         #self.laser_timestamp = time.perf_counter()
         #self.get_logger().info(str(self.laser_timestamp))
-        self.get_logger().info('Laser')
+        #self.get_logger().info('Laser')
 
     def grid_point(self, n):
         return round(n/self.grid_resolution)*self.grid_resolution
 
-    def polar_to_cartesian_global(self):
+    def calculate_cartesian_coordinates(self, robot_ranges, robot_x_position, robot_y_position, robot_yaw):
         
         x = []
         y = []
@@ -67,21 +104,11 @@ class MapCreator(Node):
 
         x_ps = []
         y_ps = []
-
         
-        if self.ranges == None or self.pose == None:
-            self.get_logger().info('Não foi possível identificar a pose e/ou dados do laser do robô')
-            return None, None, None, None, None
-
-        self.get_logger().info(str(abs(self.pose_timestamp - self.laser_timestamp)))
-
-        if abs(self.pose_timestamp - self.laser_timestamp) > 0.01:
-            return None, None, None, None, None
-        
-        yaw     = self.yaw
-        x_p     = self.position.x
-        y_p     = self.position.y
-        ranges_laser  = np.array(self.ranges)
+        yaw = robot_yaw
+        x_p = robot_x_position
+        y_p = robot_y_position
+        ranges_laser = robot_ranges
 
         R1 = np.matrix([[1, 0, x_p] , [0, 1, y_p], [0, 0, 1]])
         R2 = np.matrix([[math.cos(yaw), -math.sin(yaw), 0] , [math.sin(yaw), math.cos(yaw), 0], [0, 0, 1]])
@@ -109,52 +136,163 @@ class MapCreator(Node):
                         x_ps.append(P[0,0])
                         y_ps.append(P[1,0])
 
-                        weight.append(int(0))
+                        weight.append(int(1))
                     else:
-                        weight.append(int(100))
+                        weight.append(int(-1))
                 else:
                     P  = np.matrix([[const * 3.5 * math.cos(math.radians(i/self.laser_const))],[const * 3.5 * math.sin(math.radians(i/self.laser_const))],[1]])
                     
                     x.append(self.grid_point((np.dot(M,P))[0,0]))
                     y.append(self.grid_point((np.dot(M,P))[1,0]))
 
-                    weight.append(int(100))
+                    weight.append(int(-1))
         
-        
-        #t = pd.DataFrame(data={'x':x_ps, 'y':y_ps})
-        #t.to_csv('teste')
-        d = {'x':x, 'y':y, 'w':weight}
-
-        if self.teste:
-            self.dt = pd.DataFrame(data=d)
-            self.teste = False
-        else:
-            dt2 = pd.DataFrame(data=d)
-            self.dt = pd.concat([self.dt,dt2], ignore_index=True)
-
-        #self.dt.to_csv('Pontos_mapa.csv')
-        plt.scatter(self.dt['x'], self.dt['y'], s=1, c = self.dt['w'])
-        plt.scatter(x_p , y_p, color='green', s=30)
-
-        #plt.savefig('myimage.svg', format='svg', dpi=500)
-        
-        plt.show()
-        #self.get_logger().info('yaw = ' + str(round(yaw, 3)) + ', x = ' + str(round(x_p, 2)) + ', y = ' + str(round(y_p, 2)))
         return x, y, weight, degree, ranges
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def publish_map(self, x, y, weigths):
+
+        min_x = np.min(x)
+        min_y = np.min(y)
+        max_x = np.max(x)
+        max_y = np.max(y)
+
+        x_new = np.array(x) + np.abs(min_x)
+        y_new = np.array(y) + np.abs(min_y)
+
+        # print(len(x_new))
+        # print(len(x))
+        print(int(np.max(x_new)/self.resolution))
+        print(int(np.max(y_new)/self.resolution))
+        print(np.min(x_new))
+        print(np.min(y_new))
+
+        #local_width = math.ceil((max_x - min_x)/self.resolution)
+        #local_height = math.ceil((max_y - min_y)/self.resolution)
+        local_width = int(np.max(x_new)/self.resolution)+1
+        local_height = int(np.max(y_new)/self.resolution)+1
+
+        local_aux_map = np.zeros((local_width, local_height))
+        print(local_aux_map.shape)
+
+        # for i in range(len(x_new)):
+        #     local_aux_map[int(x_new[i]/self.resolution), int(y_new[i]/self.resolution)] = weigths[i]
+        
+        for i in range(len(x_new)):
+            x_index = int(x_new[i] / self.resolution)
+            y_index = int(y_new[i] / self.resolution)
+
+            # Check if indices are within bounds
+            if x_index >= 0 and x_index < local_width and y_index >= 0 and y_index < local_height:
+                local_aux_map[x_index, y_index] = weigths[i]
+            else:
+                print(x_index, y_index)
+
+        #self.map = None
+
+        if self.map is None:
+
+            self.map = local_aux_map
+
+            width  = local_width
+            height = local_height
+
+            self.min_x_map = min_x
+            self.min_y_map = min_y
+            self.max_x_map = max_x
+            self.max_y_map = max_y
+        
+        else:
+            lower_x = np.min([min_x, self.min_x_map])
+            higher_x = np.max([max_x, self.max_x_map])
+            lower_y = np.min([min_y, self.min_y_map])
+            higher_y = np.max([max_y, self.max_y_map])
+
+            width = math.ceil((higher_x - lower_x)/self.resolution)+1
+            height = math.ceil((higher_y - lower_y)/self.resolution)+1
+
+            aux_map = np.zeros((width, height))
+
+            relative_y_min = math.ceil(abs(self.min_y_map - min_y)/self.resolution)
+            relative_x_min = math.ceil(abs(self.min_x_map - min_x)/self.resolution)
+
+            if self.min_x_map < min_x:
+                if self.min_y_map < min_y:
+
+                    aux_map[0:self.map.shape[0], 0:self.map.shape[1]] = self.map
+                    aux_map[relative_x_min:(relative_x_min+local_aux_map.shape[0]), relative_y_min:(local_aux_map.shape[1]+relative_y_min)] = aux_map[relative_x_min:(relative_x_min+local_aux_map.shape[0]), relative_y_min:(local_aux_map.shape[1]+relative_y_min)] + local_aux_map
+
+                else:
+                    aux_map[0:self.map.shape[0], relative_y_min:(self.map.shape[1]+relative_y_min)] = self.map
+                    aux_map[relative_x_min:(relative_x_min+local_aux_map.shape[0]), 0:local_aux_map.shape[1]] = aux_map[relative_x_min:(relative_x_min+local_aux_map.shape[0]), 0:local_aux_map.shape[1]] + local_aux_map
+            else:
+                if self.min_y_map < min_y:
+                    aux_map[relative_x_min:(relative_x_min+self.map.shape[0]), 0:self.map.shape[1]] = self.map
+                    aux_map[0:local_aux_map.shape[0], relative_y_min:(local_aux_map.shape[1]+relative_y_min)] = aux_map[0:local_aux_map.shape[0], relative_y_min:(local_aux_map.shape[1]+relative_y_min)] + local_aux_map
+
+                else:
+                    aux_map[relative_x_min:(relative_x_min+self.map.shape[0]), relative_y_min:(self.map.shape[1]+relative_y_min)] = self.map
+                    aux_map[0:local_aux_map.shape[0], 0:local_aux_map.shape[1]] = aux_map[0:local_aux_map.shape[0], 0:local_aux_map.shape[1]] + local_aux_map
+
+            self.map = aux_map
+        print('oi')
+        #plt.scatter(x, y, c=weigths)
+        print(np.min((self.sigmoid(self.map)*100).astype(np.int8)))
+        print(np.max((self.sigmoid(self.map)*100).astype(np.int8)))
+        plt.imshow(((self.sigmoid(self.map)-0.5)*100).astype(np.int8), cmap='gray', origin='lower', vmin=-100,vmax=100)
+        plt.show()
+        map_1d = np.array(self.sigmoid(self.map.reshape(-1))*100).astype(np.int8)
+
+        msg = OccupancyGrid()
+        msg.header.frame_id = 'map'
+        msg.info.map_load_time.sec = 0
+        msg.info.resolution = self.resolution  # Cell size in meters
+        msg.info.width = width  # Number of cells in the x-direction
+        msg.info.height = height  # Number of cells in the y-direction
+        msg.info.origin.position.x = 0.0
+        msg.info.origin.position.y = 0.0
+        msg.info.origin.position.z = 0.0
+        msg.info.origin.orientation.x = 0.0
+        msg.info.origin.orientation.y = 0.0
+        msg.info.origin.orientation.z = 0.0
+        msg.info.origin.orientation.w = 1.0
+        msg.data = map_1d.tolist()
+
+        self.GridMap_publisher.publish(msg)
+        
+
+
+    def loop(self):
+        if self.ranges is None or self.pose is None:
+            self.get_logger().info('Unable to identify the pose and/or laser data of the robot')
+            return None
+
+        #self.get_logger().info(str(abs(self.pose_timestamp - self.laser_timestamp)))
+
+        if abs(self.pose_timestamp - self.laser_timestamp) > 0.1:
+            return None
+        
+        x, y, weight, degree, ranges = self.calculate_cartesian_coordinates(robot_ranges=np.array(self.ranges),  robot_x_position=self.position.x, robot_y_position=self.position.y, robot_yaw=self.yaw)
+        self.publish_map(x, y, weight)
 
 def main(args=None):
     rclpy.init(args=args)
     try:
-        node = MapCreator()
+        Map_Creator_node = MapCreator()
+        Map_Visualizaer_node = MapVisualizer()
 
-        executor = MultiThreadedExecutor(num_threads=3)
-        executor.add_node(node=node)
+        executor = SingleThreadedExecutor() #MultiThreadedExecutor(num_threads=4)
+        executor.add_node(node=Map_Creator_node)
+        executor.add_node(node=Map_Visualizaer_node)
 
         try: 
             executor.spin()
         finally:
             executor.shutdown()
-            node.destroy_node()
+            Map_Creator_node.destroy_node()
+            Map_Visualizaer_node.destroy_node()
 
     finally:
         rclpy.shutdown()
